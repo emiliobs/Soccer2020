960a89f31726958b23d4e9a3e58bd45e775f8cbb
Emilio <barrera_emilio@yahoo.es>
2020-02-29 22:13:57 +0000

    Show tournaments on app Xamarin

---
+++ Soccer.Prism/Soccer.Prism/ViewModels/TournamentsPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Navigation;
+using Soccer.Common.Models;
+using Soccer.Common.Service;
+using System;
+using System.Collections.Generic;
+
+namespace Soccer.Prism.ViewModels
+{
+    public class TournamentsPageViewModel : ViewModelBase
+    {
+
+        private List<TournamentResponse> _tournaments;
+        private readonly IApiService _apiService;
+
+
+        public TournamentsPageViewModel(INavigationService navigationService, IApiService apiService) : base(navigationService)
+        {
+            Title = "Soccer";
+            this._apiService = apiService;
+
+            LoadTournamentAsync();
+        }
+
+        public List<TournamentResponse> Tournaments
+        {
+            get { return _tournaments; }
+            set => SetProperty(ref _tournaments, value);
+        }
+
+        private async void LoadTournamentAsync()
+        {
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var response = await _apiService.GetListAsync<TournamentResponse>(url,"/api","/Tournaments");
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error",response.Message,"Accept");
+            }
+
+            Tournaments = (List<TournamentResponse>)response.Result;
+        }
+
+
+
+    }
+}
