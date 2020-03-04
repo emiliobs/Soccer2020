5e900a9357ca80809117e6b2762aea5b5f4a8ec1
Emilio <barrera_emilio@yahoo.es>
2020-03-04 02:31:41 +0000

    Add Master Details Page

---
+++ Soccer.Prism/Soccer.Prism/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using Soccer.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soccer.Prism.ViewModels
+{
+    public class MenuItemViewModel : Menu
+    {
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectMenuCommand;
+
+        public MenuItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public DelegateCommand SelectMenuCommand => _selectMenuCommand ?? (_selectMenuCommand = new DelegateCommand(SelectMenuAsync));
+
+        private async void SelectMenuAsync()
+        {
+            await _navigationService.NavigateAsync($"/SoccerMasterDetailPage/NavigationPage/{PageName}");
+        }
+
+    }
+}
